5ab0482d13dc48551dcc6e5a58de271a339c355e
chiscookeke11 <chiscookeke11@gmail.com>
2025-05-08 07:30:14 +0100

    fix: fixed UI of the faq section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,10 +1,8 @@
-import Carousel from "@/layout/Carousel";
 import FaqSection from "@/layout/faq-section";
 
 export default function Home() {
   return (
     <div>
-      <Carousel />
       <FaqSection />
     </div>
   );
