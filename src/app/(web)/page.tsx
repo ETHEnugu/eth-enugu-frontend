8782a44f2e638951c16d4733c3cb6918a201c0a7
chiscookeke11 <chiscookeke11@gmail.com>
2025-05-09 07:29:53 +0100

    fix

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,20 +1,9 @@
-import AboutSection from "@/components/ui/AboutSection";
-import Banner from "@/components/ui/Banner";
-import Banner2 from "@/components/ui/Banner2";
-import InfoSection from "@/components/ui/InfoSection";
 import FaqSection from "@/layout/faq-section";
 
 export default function Home() {
   return (
-    <>
-      <AboutSection />
-      <InfoSection />
-      <Banner />
-      <Banner2
-        title="Mentor/Speak at EthEnugu '25"
-        content="Apply to mentor rising talent and make an impact in the Southeast tech ecosystem."
-      />
+    <div>
       <FaqSection />
-    </>
+    </div>
   );
 }
