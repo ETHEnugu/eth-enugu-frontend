1ef670432888f7bd8a73183b2876ce098278cd97
chiscookeke11 <chiscookeke11@gmail.com>
2025-05-12 13:59:11 +0100

    feat: added the gallery section

--- src/components/common/galleryCard/index.tsx
+++ src/components/common/galleryCard/index.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
 
-export default function GalleryCard() {
+interface GalleryCardProp {
+  img: string;
+  deg: string;
+  marginTop: string;
+}
+export default function GalleryCard({ img, deg, marginTop }: GalleryCardProp) {
   return (
-    <div className="w-[188.77px] h-[194.32px] md:w-[288px] md:h-[360px] flex flex-col gap-2.5 py-2.5 px-2 relative">
+    <div
+      className={`w-[188.77px] h-[194.32px] md:w-[288px] md:h-[360px] flex flex-col gap-2.5 py-2.5 px-2 relative bg-[var(--background)] mx-1.5 md:mx-6 transform  `}
+      style={{
+        transform: `rotate(${deg})`,
+        marginTop: marginTop,
+      }}
+    >
       <div className="w-full h-[78%] relative overflow-hidden">
-        <Image
-          src="/enugu header image.png"
-          alt="gallery image"
-          fill
-          className="object-cover"
-        />
+        <Image src={img} alt="gallery image" fill className="object-cover" />
       </div>
     </div>
   );
