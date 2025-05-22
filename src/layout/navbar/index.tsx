5b8f6db3767e4923206bed3356c714883105a819
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2025-05-22 11:49:52 +0100

    feat: build home hero page for desktop (#17)
    * feat: build home hero page for desktop
    
    * feat: build hero page and optinmise for mobile with mobile nav

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -5,10 +5,13 @@ import Link from "next/link";
 import { Button } from "@/components/common/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import MobileNav from "./MobileNav";
 
 export default function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
     <nav className="w-full flex items-center px-6 py-4 bg-white/20 border-b border-gray-400">
@@ -22,13 +25,15 @@ export default function Navbar() {
             title="Eth Enugu"
             aria-label="Eth Enugu"
             aria-labelledby="Eth Enugu"
-            // fill
             className="w-full h-full"
           />
         </Link>
       </div>
 
-      <div role="navigation" className="flex items-center gap-4 ml-18">
+      <div
+        role="navigation"
+        className="hidden md:flex items-center gap-4 ml-18"
+      >
         {navbar_items.map((item, idx) => (
           <Link
             key={idx}
@@ -42,7 +47,7 @@ export default function Navbar() {
         ))}
       </div>
 
-      <aside className="w-max ml-auto flex items-center gap-4">
+      <aside className="w-max ml-auto hidden md:flex items-center gap-4">
         <Button type="button" variant="plain" design="rounded">
           Register for event
         </Button>
@@ -57,6 +62,16 @@ export default function Navbar() {
           <Icon icon="solar:arrow-right-linear" width={18} height={18} />
         </Button>
       </aside>
+
+      <button
+        type="button"
+        className="md:hidden block ml-auto cursor-pointer"
+        onClick={() => setIsMobileMenuOpen(true)}
+      >
+        <Icon icon="prime:bars" width={32} height={32} />
+      </button>
+
+      <MobileNav isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} />
     </nav>
   );
 }
